d49d1bd3cb758e6718c09fccf221080e038998b6
Muhaz <muhazvla313@gmail.com>
2022-11-04 10:32:04 +0530

    Duplicate ele

---
+++ Array/dupEle.js
@@ -0,0 +1,20 @@
+// remove duplicate ele from a given array
+
+function solution(array) {
+  let exists = {}
+  let newArr = []
+  let ele
+  for (let i = 0; i < array.length; i++) {
+    ele = array[i]
+    if (!exists[ele]) {
+        newArr.push(ele)
+        exists[ele] = true
+    }
+  }
+  return newArr
+}
+var result = solution([1,2,2,3,4,4,5]);
+console.log(result);
+
+// time complexity = O(n)
+//space complexity = O(n^2)
